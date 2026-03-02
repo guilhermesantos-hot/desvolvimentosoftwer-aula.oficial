6066c9a5d8c5d236876f135fd38225085f74b5c4
guilhermesantos-hot <guilhermesantosfrancisco@hotmail.com>
2026-03-02 10:17:37 -0300

    Delete carro.softwer directory

--- carro.softwer/db.py
+++
@@ -1,4 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy   # DE flask_sqlalchemy IMPORTAR SQLAlchemy:
-                                          # Extensão do Flask que integra o ORM SQLAlchemy para manipular bancos de dados
-
-db = SQLAlchemy()                         # Cria uma instância do SQLAlchemy (objeto que gerencia a conexão, tabelas e sessões do banco)
