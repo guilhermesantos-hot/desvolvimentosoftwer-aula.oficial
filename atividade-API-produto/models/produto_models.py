3ad33e763e4dc0b84586fb41191b5b129ae5c54e
guilhermesantos-hot <guilhermesantosfrancisco@hotmail.com>
2026-03-10 11:42:38 -0300

    Add files via upload

--- atividade-API-produto/models/produto_models.py
+++ atividade-API-produto/models/produto_models.py
@@ -3,18 +3,13 @@ from db import db
 class Produto(db.Model):
     __tablename__ = 'produto'
 
-    id = db.Column(db.Integer, primary_Key=True)
-    nome = db.Column(db.String(80), nullable=False)
-    categoria = db.Column(db.String(80), nullable=False)
-    preco = db.Column(db.Integer, nullable=False)
-     
-    def json(self):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100))
+    preco = db.Column(db.Float)
+
+    def to_dict(self):
         return {
-            'id': self.id,
-            'nome': self.nome, 
-            'categoria': self.categoria, 
-            'preco': self.preco
+            "id": self.id,
+            "nome": self.nome,
+            "preco": self.preco
         }
-    
-    
-    
